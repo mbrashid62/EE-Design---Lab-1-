3565abee57cc26caf0bd3123f139cbbadde593d7
Yujun Huang <yujun-huang@uiowa.edu>
2015-09-10 15:29:47 -0500

    Add Jars

--- EE-Design-Lab-One/src/Thermometer.java
+++ EE-Design-Lab-One/src/Thermometer.java
@@ -22,6 +22,9 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
+import javax.swing.UIManager;
 
 public class Thermometer {
 
@@ -65,18 +68,15 @@ public class Thermometer {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 400, 468);
+		frame.getContentPane().setBackground(new Color(102, 204, 204));
+		frame.getContentPane().setForeground(new Color(0, 0, 0));
+		frame.setBounds(100, 100, 387, 468);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setEditable(false);
-		textArea.setBounds(52, 155, 261, 221);
-		frame.getContentPane().add(textArea);
-		
 		
 		JLabel lblTextMessageAlert = new JLabel("Text Message Alert");
-		lblTextMessageAlert.setBounds(0, 0, 358, 22);
+		lblTextMessageAlert.setBounds(0, 0, 347, 22);
 		lblTextMessageAlert.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTextMessageAlert.setFont(new Font("Tahoma", Font.BOLD, 15));
 		frame.getContentPane().add(lblTextMessageAlert);
@@ -90,11 +90,12 @@ public class Thermometer {
 		frame.getContentPane().add(label);
 		
 		textField = new JTextField();
-		textField.setBounds(107, 33, 175, 22);
+		textField.setBounds(107, 33, 133, 22);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		JButton btnSave = new JButton("Save");
+		btnSave.setBackground(new Color(153, 255, 255));
 		btnSave.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if (textField.getText().matches("[0-9]+") && textField.getText().length() > 2) {
@@ -104,41 +105,42 @@ public class Thermometer {
 				}
 			}
 		});
-		btnSave.setBounds(296, 33, 77, 23);
+		btnSave.setBounds(270, 33, 77, 23);
 		frame.getContentPane().add(btnSave);
 		
 		JLabel lblNewLabel = new JLabel("Real-Time Temperature");
-		lblNewLabel.setBounds(83, 130, 203, 14);
+		lblNewLabel.setBounds(84, 130, 186, 14);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Temperature Settings");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNewLabel_1.setBounds(96, 62, 159, 22);
+		lblNewLabel_1.setBounds(101, 62, 159, 22);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblMax = new JLabel("Max:");
 		lblMax.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblMax.setBounds(52, 99, 34, 14);
+		lblMax.setBounds(30, 99, 34, 14);
 		frame.getContentPane().add(lblMax);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(88, 95, 60, 24);
+		textField_1.setBounds(62, 95, 60, 24);
 		frame.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Min:");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblNewLabel_2.setBounds(168, 100, 34, 14);
+		lblNewLabel_2.setBounds(143, 99, 34, 15);
 		frame.getContentPane().add(lblNewLabel_2);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(201, 95, 60, 22);
+		textField_2.setBounds(177, 95, 60, 24);
 		frame.getContentPane().add(textField_2);
 		textField_2.setColumns(10);
 		
 		JButton btnNewButton = new JButton("Save");
+		btnNewButton.setBackground(new Color(153, 255, 255));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				int a = Integer.parseInt(textField_1.getText());
@@ -146,41 +148,69 @@ public class Thermometer {
 				if(a <= b) {
 					JOptionPane.showMessageDialog(null,"Max Temperature must be bigger than Min Temperature!");
 				} else {
-					maxTemp = a;
-					minTemp = b;
+					if(a>63||b<-10) {
+						JOptionPane.showMessageDialog(null,"Proper temperature limit should be within -10 ~ 63 degree Cesius!");
+					} else {
+						maxTemp = a;
+						minTemp = b;
+					}	
 				}
 				
 			}
 		});
 		System.out.print(targetNumber);
-		btnNewButton.setBounds(296, 96, 77, 23);
+		btnNewButton.setBounds(270, 96, 77, 23);
 		frame.getContentPane().add(btnNewButton);
 		
 		ButtonGroup group = new ButtonGroup();
 		
 		JRadioButton rdbtnC = new JRadioButton("C \u00BA");
+		rdbtnC.setBackground(new Color(102, 204, 204));
 		rdbtnC.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		rdbtnC.setBounds(319, 173, 46, 23);
+		rdbtnC.setBounds(301, 170, 46, 23);
+		rdbtnC.setSelected(true);
 		frame.getContentPane().add(rdbtnC);
 		
 		JRadioButton rdbtnF = new JRadioButton("F \u00BA");
+		rdbtnF.setBackground(new Color(102, 204, 204));
 		rdbtnF.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		rdbtnF.setBounds(319, 199, 46, 23);
+		rdbtnF.setBounds(301, 196, 46, 23);
 		frame.getContentPane().add(rdbtnF);
 		
 		group.add(rdbtnC);
 		group.add(rdbtnF);
 		
 		JToggleButton tglbtnNewToggleButton = new JToggleButton("LED");
-		tglbtnNewToggleButton.setBounds(323, 295, 50, 23);
+		tglbtnNewToggleButton.setBackground(new Color(255, 255, 255));
+		tglbtnNewToggleButton.setFont(new Font("Tahoma", Font.BOLD, 11));
+		tglbtnNewToggleButton.setForeground(new Color(102, 0, 102));
+		tglbtnNewToggleButton.setBounds(301, 313, 60, 31);
 		frame.getContentPane().add(tglbtnNewToggleButton);
 		
 		JButton btnNewButton_1 = new JButton("Go");
-		btnNewButton_1.setBounds(48, 387, 89, 23);
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnNewButton_1.setBackground(new Color(153, 255, 255));
+		btnNewButton_1.setForeground(new Color(0, 153, 51));
+		btnNewButton_1.setBounds(30, 387, 89, 23);
 		frame.getContentPane().add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Stop");
+		btnNewButton_2.setBackground(new Color(153, 255, 255));
+		btnNewButton_2.setForeground(new Color(255, 51, 51));
+		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 11));
 		btnNewButton_2.setBounds(193, 387, 89, 23);
 		frame.getContentPane().add(btnNewButton_2);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 155, 285, 217);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBackground(new Color(204, 255, 153));
+		textArea.setForeground(new Color(51, 102, 204));
+		textArea.setBounds(0, 0, 285, 217);
+		panel.add(textArea);
+		textArea.setFont(new Font("Comic Sans MS", textArea.getFont().getStyle() | Font.ITALIC, textArea.getFont().getSize() + 140));
 	}
 }
