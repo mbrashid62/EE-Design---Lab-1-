e62e7186a1a6db3a9c562dac0e6362f719ae295a
Bill Rashid <mohammad-rashid@uiowa.edu>
2015-09-22 22:20:28 -0500

    Added Graph Implementation

--- EE-Design-Lab-One/src/Graph.java
+++ EE-Design-Lab-One/src/Graph.java
@@ -11,63 +11,94 @@ import java.io.IOException;
 import javax.swing.JFrame;
 
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class Graph.
- */
 public class Graph {
 
-	
-   /** The frame. */
-   private JFrame frame;
-
-    /**
-     * Instantiates a new graph class.
-     */
+    private int secondTracker;
+    private boolean shouldWriteNewLine;
+    private String[] jsonElements;
+    private String jsonToSend;
+    private boolean isFirstElement;
     public Graph(){
+    	
+    	secondTracker = 0;
+    	shouldWriteNewLine = false;
+    	jsonElements = new String[300];
+    	isFirstElement = true;
     
     	 try {
              String url = "http://localhost:63342/Lab_One/main.html";
              java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
            }
            catch (java.io.IOException e) {
-               System.out.println(e.getMessage());
+               System.out.println("Trouble with opening HTML file: " + e.getMessage());
            }
-
     }
     
-    /**
-     * Send data.
-     *
-     * @param temp the temp
-     */
-    void sendData(double temp){
+    void sendToGraph (double temp){
+    	
     	 //write data to file
 		try {
 
 			System.out.println("Read temp: " + temp);
-			String content = "Test data";
-
-			File file = new File("H://Lab_One/temps/temps.txt");
+			String jsonElement;
+			String openArray = "[";
+			String closeArray = "]";
+			
+			//if 300th second reset counter and don't add comma to json el
+			if(secondTracker == 300){
+				secondTracker = 0;
+				jsonElement =  "{" + " \"seconds\": " + secondTracker + ',' + " \"temperature\" : " + temp + "}" + "\n";
+				shouldWriteNewLine = false;
+			}
+			
+			if(isFirstElement){
+				jsonElement = "{" + " \"seconds\": " + secondTracker + ',' + " \"temperature\" : " + temp + "}" + "\n";
+				isFirstElement = false;
+			}
+			//else add json entry as normal
+			else{
+				jsonElement = ",{" + " \"seconds\": " + secondTracker + ',' + " \"temperature\" : " + temp + "}" + "\n";
+			}
+			
+			//store json entry in array
+			jsonElements[secondTracker] = jsonElement;
+			
+			//concatenate all array contents and add array brackets for subsequent file write
+			jsonToSend =  "[" + addArrayToString(jsonElements) + "]";
+			
+			System.out.println("jsonToSend: " + jsonToSend);
+			File file = new File("H://Lab_One/temps/temps.json");
 
 			// if file doesn't exists, then create it
-			if (!file.exists()) {
-				
+			if (!file.exists()) {	
 				file.createNewFile();
 			}
 
-			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			FileWriter fw = new FileWriter(file.getAbsoluteFile(), false);
 			BufferedWriter bw = new BufferedWriter(fw);
-			bw.write(content);
+			bw.write(jsonToSend);
 			bw.close();
 
-			System.out.println("Done");
+			System.out.println("Done writing to file");
 
+			secondTracker++;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
     }
 
+	private String addArrayToString(String[] jsonElements2) {
+		
+		int count = 0;
+		String result = "";
+		while(jsonElements2[count] != null)
+		{
+		    result = result + jsonElements2[count];
+		    count++;
+		}
+		return result;
+	}
 
+   
 	
 }
